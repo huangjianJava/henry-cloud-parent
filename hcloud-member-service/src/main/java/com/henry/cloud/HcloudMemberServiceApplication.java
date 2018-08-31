c7a430bfb3f17d6b703533c15d339781f7ef023b
huangjian <huangjian@sinter.cn>
2018-08-31 19:46:41 +0800

    first commit

---
+++ hcloud-member-service/src/main/java/com/henry/cloud/HcloudMemberServiceApplication.java
@@ -0,0 +1,12 @@
+package com.henry.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcloudMemberServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcloudMemberServiceApplication.class, args);
+	}
+}
